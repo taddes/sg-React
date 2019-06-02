690329ade24248a05ccdba2d7b50ab0d913cdd6b
Taddes Korris <taddes.korris@gmail.com>
2019-06-01 20:45:51 -0400

    props and reusable comps

--- components/src/index.js
+++ components/src/index.js
@@ -8,11 +8,23 @@ const App = () => {
   return (
     <div className="ui container comments">
     <ApprovalCard>
+      <div>
+        <h4>Warning!</h4>
+        <span>Are you sure? </span>
+      </div>
+    </ApprovalCard>
+
+
+    <ApprovalCard>
       <CommentDetail author="Sam" timeAgo="Today at 4:44 PM" avatar={faker.image.avatar()} comment="Yeah, well put!"/>
     </ApprovalCard>
+    <ApprovalCard>
       <CommentDetail author="Jane" timeAgo="Today at 1:03 PM" avatar={faker.image.avatar()} comment="I'm inclined to agree, for sure!"/>
+    </ApprovalCard>
+    <ApprovalCard>
       <CommentDetail author="Ralph" timeAgo="Today at 10:34 AM" avatar={faker.image.avatar()} comment="Writing is on the wall :p"/>
-    </div>
+    </ApprovalCard>
+      </div>
   )
 };
 
