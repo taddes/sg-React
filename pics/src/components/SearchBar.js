cea34e4f1170594ffd55e4564d1c66722e75c824
Taddes Korris <taddes.korris@passportinc.com>
2019-06-12 21:15:52 -0400

    new proj

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react'
+
+
+class SearchBar extends Component {
+  state = {
+    term: ''
+  }
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.term);
+  }
+
+ 
+  
+  render() {
+    console.log(this.state);
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label htmlFor="search">Image Search</label>
+            <input type="text" value={this.state.term} onClick={this.onInputClick} onChange={(e) => this.setState({ term: e.target.value })}/>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
