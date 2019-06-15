aa2f521d3305742cd4730e04250dfb80e1bc8d81
Taddes Korris <taddes.korris@passportinc.com>
2019-06-14 21:00:37 -0400

    async api call to unsplash api

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';  
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
 
+
 class App extends Component {
-  onSearchSubmit(term) {
-    console.log(term)
+
+  componentDidMount() {
+    const API_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+    console.log(API_KEY)
+    return API_KEY
   }
+ 
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+      }
+    });
+    console.log(response.data.results)
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
