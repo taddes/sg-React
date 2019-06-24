246f39fe3e293f21f8a3f9ce4c852dfc6f4e9558
Taddes Korris <taddes.korris@gmail.com>
2019-06-23 20:27:20 -0400

    setup

---
+++ videos/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>App
+        <SearchBar />
+      </div>
+    )
+  }
+}
+
+export default App;
