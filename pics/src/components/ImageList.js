65196a4d0f4ea9b61e4f05ffb498f77d33223d6b
Taddes Korris <taddes.korris@gmail.com>
2019-06-23 20:13:30 -0400

    css grid implementation

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ImageCard from './ImageCard'
 import './ImageList.css'
 
 const ImageList = (props) => {
   console.log(props.images)
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} key={image.id} alt={image.description}/>
+    return <ImageCard key={image.id} image={image}/>
   })
   
   return(
