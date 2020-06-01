e1f2569525466c28471046721d39e0eca4fa7b72
Taddes Korris <taddes.korris@gmail.com>
2020-06-01 18:56:26 -0400

    axios conf

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -34,33 +34,5 @@ class SearchBar extends Component {
     );
   }
 }
-// class SearchBar extends Component {
-//   state = { searchTerm: ''};
-
-//   onInputChange = (event) => {
-//     this.setState({ searchTerm: event.target.value })
-//   }
-
-//   onFormSubmit = (event) => {
-//     event.preventDefault();
-//     // TODO make sure we clal callback from parent component
-//   }
-//   render() {
-//     return(
-//       <div className="ui segment search-bar">
-//         <form  className="ui form" onSubmit={this.onFormSubmit}>
-//           <div className="field">
-//             <label>Video Search:</label>
-//             <input 
-//               type="text" 
-//               value={this.state.searchTerm} 
-//               onChange={this.onInputChange}
-//             />
-//           </div>
-//         </form>
-//       </div>
-//     )
-//   }
-// }
 
 export default SearchBar;
