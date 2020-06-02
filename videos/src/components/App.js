ff93e80be9921139a222c2bc806383ddb792a8e6
Taddes Korris <taddes.korris@gmail.com>
2020-06-02 14:14:52 -0400

    video list component with props

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList"
 
 class App extends Component {
+  state = { videos: [] };
 
   onTermSubmit = async term => {
-    console.log(term);
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
 
-    console.log(response);
+    console.log(response.data.items);
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+
+        I have {this.state.videos.length} videos.
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
